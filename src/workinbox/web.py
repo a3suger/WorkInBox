5cd59be2543ea074395ea519cbd5b568b1af27a9
Akira SATO <akira@cc.tsukuba.ac.jp>
2026-08-11 00:53:54 +0900

    Provide self CC for schedule support requests

--- src/workinbox/web.py
+++ src/workinbox/web.py
@@ -174,6 +174,7 @@ def create_app(
             name="schedules.html",
             context={
                 "items": items,
+                "self_cc": config.identity.mailbox_address if config.identity else "",
                 **common_view_flags(schedules=True),
                 "action_message": action_message,
                 "action_failure": action_failure,
